e488d53995bda4cee519f6d60945a5c83e76395f
gauteao <99391960+gauteao@users.noreply.github.com>
2024-05-13 16:12:32 +0200

    Updated javascript, small design updates
    small update on fetching hotels, design on front page + forms

--- Project/Frontend/src/script.js
+++ Project/Frontend/src/script.js
@@ -1,26 +1,17 @@
 console.log("script.js loaded");
 
-window.onload = function () {
-    // Fetch navbar.html and insert its content into the navbar div
-    fetch('./navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        });
+function commonOnLoad() {
+        // Fetch navbar.html and insert its content into the navbar div
+        fetch('./navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                const navbarDiv = document.getElementById('navbar');
+                if (navbarDiv) {
+                    navbarDiv.innerHTML = data;
+                }
+            });
 
-    var picker = new Litepicker({
-        element: document.getElementById('date-range'),
-        singleMode: false,
-        format: 'YYYY-MM-DD',
-        delimiter: ' to ',
-        autoApply: true,
-        onSelect: function(date1, date2){
-            var checkinDate = date1.format('YYYY-MM-DD');
-            var checkoutDate = date2.format('YYYY-MM-DD');
-            // Do something with the selected dates
-        }
-    });
-
+ // Initialize the dropdown menu for login
     var dropdownContent = document.querySelector('.dropdown-content');
     var dropbtn = document.querySelector('.dropbtn');
     var timeout;
@@ -73,41 +64,3 @@ window.onload = function () {
         dropdownContent.style.left = left + 'px';
     }
 }
-document.getElementById('booking-form').addEventListener('submit', function (event) {
-    // Prevent the form from submitting normally
-    event.preventDefault();
-
-    // Get the form inputs
-    var cityPreference = document.getElementsByName('destination_preference')[0].value;
-    var dateRange = document.getElementsByName('date_range')[0].value;
-    var totalGuests = document.getElementsByName('total_guests')[0].value;
-
-    // Split the date range into checkin and checkout dates
-    var dates = dateRange.split(' to ');
-    var checkinDate = dates[0];
-    var checkoutDate = dates[1];
-
-    // Make an AJAX request to the backend endpoint
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8080/hotels/search?city=' + encodeURIComponent(cityPreference) +
-        '&checkIn=' + encodeURIComponent(checkinDate) +
-        '&checkOut=' + encodeURIComponent(checkoutDate) +
-        '&guests=' + encodeURIComponent(totalGuests));
-
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            // Parse the response JSON
-            var hotels = JSON.parse(xhr.responseText);
-
-            // Store the hotels data in sessionStorage to pass it to the next page
-            sessionStorage.setItem('hotels', JSON.stringify(hotels));
-
-            // Redirect the user to the results page
-            window.location.href = 'results.html';
-        } else {
-            console.error('Request failed. Status: ' + xhr.status);
-        }
-    };
-
-    xhr.send();
-});
