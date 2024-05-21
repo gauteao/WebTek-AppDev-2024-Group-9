ecc8bdb2b9cb328b61018a3edb08741916634851
gauteao <99391960+gauteao@users.noreply.github.com>
2024-05-21 17:20:03 +0200

    Update SecurityConfig.java

--- Project/Backend/src/main/java/no/ntnu/stayfinder/security/SecurityConfig.java
+++ Project/Backend/src/main/java/no/ntnu/stayfinder/security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -60,18 +59,6 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
             throws Exception {
         return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**").allowedOrigins("https://jazzy-taiyaki-d8ca14.netlify.app/").allowedMethods("GET", "POST", "PUT", "DELETE")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-        }
-    };
     }
 
     /**
